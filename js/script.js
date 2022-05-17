da300db5b50b4e08ab148df3da121a37292e7fd7
Marija Morozova <mmoroz@ttu.ee>
2022-05-17 17:52:17 +0300

    max's movements + platforms are added

--- js/script.js
+++ js/script.js
@@ -29,6 +29,8 @@ class Player {
 
     update() {
         this.draw();
+
+        this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
 
         //Gravity
@@ -41,12 +43,84 @@ class Player {
     }
 }
 
+class Platform {
+    constructor() {
+        this.position = {
+            x: 500,
+            y: 200
+        };
+        this.width = 200;
+        this.height = 20;
+    }
+
+    draw() {
+        context.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
+
 const player = new Player();
+
+const platform = new Platform();
+
+const keys = {
+    left: {
+        pressed: false
+    },
+    right: {
+        pressed: false
+    }
+};
+
+window.addEventListener('keydown', ({ keyCode }) => {
+    switch(keyCode) {
+        case 65: 
+            keys.left.pressed = true;
+            break;
+        case 68: 
+            keys.right.pressed = true;
+            break;
+        case 87: 
+            console.log('up');
+            player.velocity.y -= 20;
+            break;
+        case 83: 
+            console.log('down');
+            break;
+    }
+});
+
+window.addEventListener('keyup', ({ keyCode }) => {
+    switch(keyCode) {
+        case 65: 
+            keys.left.pressed = false;
+            break;
+        case 68: 
+            keys.right.pressed = false;
+            break;
+    }
+});
 
 function animate() {
     requestAnimationFrame(animate);
     context.clearRect(0, 0, canvas.width, canvas.height);
     player.update();
+    platform.draw();
+
+    if (keys.right.pressed) {
+        player.velocity.x = 5;
+    }
+    else if (keys.left.pressed) {
+        player.velocity.x = -5;
+    }
+    else {
+        player.velocity.x = 0;
+    }
+
+    //Platform Collision Detection
+    if (player.position.y + player.heigth <= platform.position.y && player.position.y + player.heigth + player.velocity.y >= platform.position.y &&
+        player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
+        player.velocity.y = 0;
+    }
 }
 
 animate();
